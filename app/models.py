196decd1213fdde4342107651934f659f1a1de35
Ben Botsford <ben.botsford@flatironschool.com>
2022-11-04 13:55:30 -0500

    rename columns

--- app/models.py
+++ app/models.py
@@ -19,19 +19,19 @@ Base = declarative_base()
 class Student(Base):
     __tablename__ = 'students'
     __table_args__ = (
-        UniqueConstraint('student_email',
+        UniqueConstraint('email',
             name='unique_email'),
-        CheckConstraint('student_grade BETWEEN 1 AND 12',
+        CheckConstraint('grade BETWEEN 1 AND 12',
             name='grade_between_1_and_12'))
 
-    student_id = Column(Integer(), primary_key=True)
-    student_name = Column(String(), index=True)
-    student_email = Column(String(55))
-    student_grade = Column(Integer())
-    student_birthday = Column(DateTime())
-    student_enrolled_date = Column(DateTime(), default=datetime.now())
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(), index=True)
+    email = Column(String(55))
+    grade = Column(Integer())
+    birthday = Column(DateTime())
+    enrolled_date = Column(DateTime(), default=datetime.now())
 
     def __repr__(self):
-        return f"Student {self.student_id}: " \
-            + f"{self.student_name}, " \
-            + f"Grade {self.student_grade}"
+        return f"Student {self.id}: " \
+            + f"{self.name}, " \
+            + f"Grade {self.grade}"
